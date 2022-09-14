66c35d1b4c0080ffc08448c85d24d81c3786e2be
duke <duke@trymuse.com>
2022-09-14 08:15:42 +0100

    initial commit, setting up layout

---
+++ android/app/src/main/kotlin/com/dcdevs/freshcut/freshcut/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dcdevs.freshcut.freshcut
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/painting.dart';
+
+const Color lightOrange = Color(0xFFF2BC3D);
+const Color darkOrange = Color(0xFFD93636);
+const Color goldYellow = Color(0xFFF2BC3D);
+const Color grey1 = Color(0xFF504C57);
+const Color grey2 = Color(0xFF645F6D);
+const Color grey3 = Color(0xFF8C8797);
+const Color grey4 = Color(0xFFA19DAA);
